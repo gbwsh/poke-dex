9bacf080df73f8db5c2650c4c2d5cc4cbac4cbe1
gbwsh <kustrabartosz99@gmail.com>
2022-05-19 10:16:14 +0200

    updated pokemonlist component

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,8 +1,20 @@
-import { Link, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+// const PokemonList = ({ pokemonResults }) => {
+//   return pokemonResults ? (
+//     pokemonResults.map((pokemon) => {
+//       const id = pokemon.url.split("/")[6];
+//       console.log(id);
+//       <Link to={`/details/${id}`}>{pokemon.name}</Link>;
+//     })
+//   ) : (
+//     <h1>loading pokemans...</h1>
+//   );
+// };
 
 const PokemonList = ({ pokemonResults }) => {
   return pokemonResults ? (
-    pokemonResults.map((pokemon) => {
+    pokemonResults.forEach((pokemon) => {
       const id = pokemon.url.split("/")[6];
       console.log(id);
       <Link to={`/details/${id}`}>{pokemon.name}</Link>;
